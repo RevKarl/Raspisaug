0f3c54f18a5c540b0cbb848249b91766a6c025c0
RevKarl <csainzmartinez@gmail.com>
2013-12-11 12:08:14 +0000

    2

--- Raspisaug/mainwindow.h
+++ Raspisaug/mainwindow.h
@@ -1,7 +1,9 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
+//#include <QMainWindow>
+#include <QPushButton>
+#include <QApplication>
 
 namespace Ui {
 class MainWindow;
@@ -18,5 +20,14 @@ public:
 private:
     Ui::MainWindow *ui;
 };
+
+Click::Click(QWidget *parent)
+    : QWidget(parent)
+{
+  QPushButton *quit = new QPushButton("Quit", this);
+  quit->setGeometry(50, 40, 75, 30);
+
+  connect(quit, SIGNAL(clicked()), qApp, SLOT(quit()));
+}
 
 #endif // MAINWINDOW_H
